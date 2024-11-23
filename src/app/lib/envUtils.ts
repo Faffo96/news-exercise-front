c16784fc3501a7377c0b4533d53552ca947278f8
Fabio Scaramozzino <fabioscar96@gmail.com>
2024-11-23 14:24:02 +0100

    smallfix

--- src/app/lib/envUtils.ts
+++ src/app/lib/envUtils.ts
@@ -3,8 +3,8 @@ export function validateEnvVariables(): boolean {
     if (!process.env.NEXT_PUBLIC_BACKEND_BASE_URL) {
         console.error('Backend base URL env is not defined.');
         return false;
-    } else if (!process.env.NEXTAUTH_URL) {
+    } /* else if (!process.env.NEXTAUTH_URL) {
         console.error('Redirect uri env is not defined.');
-    }
+    } */
     return true;
 }
